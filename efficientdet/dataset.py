7791d2e15bffecc7333653ed1bea103cb37d5595
josdo <joshuadong.usa@gmail.com>
2020-11-10 02:32:36 +0000

    first time train effdet, convert voc format to int id coco

--- efficientdet/dataset.py
+++ efficientdet/dataset.py
@@ -47,9 +47,12 @@ class CocoDataset(Dataset):
         return sample
 
     def load_image(self, image_index):
+#         print(image_index)
         image_info = self.coco.loadImgs(self.image_ids[image_index])[0]
-        path = os.path.join(self.root_dir, self.set_name, image_info['file_name'])
+        filename = str(image_info['id']) + '.jpg'
+        path = os.path.join(self.root_dir, self.set_name, filename) # TODO should be image_info['filename'], which doesn't follow the new ids
         img = cv2.imread(path)
+#         print(img.shape)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
         return img.astype(np.float32) / 255.
